ef5b96ed63622ee330288b6386645c5925f1185a
İsmet Ataklı <ismetatakli@gmail.com>
2022-05-12 10:24:38 +0300

    OtherApi controllers added and updated

---
+++ OtherApp.API/Controllers/StockController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace OtherApp.API.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StockController : ControllerBase
+    {
+        public IActionResult GetStock()
+        {
+            var userName = HttpContext.User.Identity.Name;
+            var userId = User.Claims.FirstOrDefault(x => x.Type == ClaimTypes.NameIdentifier);
+
+            return Ok($"Stock --- username:{userName} - userid:{userId}");
+
+        }
+    }
+}
---
+++ OtherApp2.API/Controllers/InvoiceController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace OtherApp2.API.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class InvoiceController : ControllerBase
+    {
+        public IActionResult Get()
+        {
+            var userName = HttpContext.User.Identity.Name;
+            var userId = User.Claims.FirstOrDefault(x=>x.Type == ClaimTypes.NameIdentifier);
+
+            return Ok($"Invoice --- username:{userName} - userid:{userId}");
+        }
+    }
+}
